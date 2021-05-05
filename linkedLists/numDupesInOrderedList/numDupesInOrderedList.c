9483f01fb04221f8c1cb56a1ae7c5d2a53b10b09
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-05 22:22:01 +1000

    Complete duplicates exercise

--- linkedLists/numDupesInOrderedList/numDupesInOrderedList.c
+++ linkedLists/numDupesInOrderedList/numDupesInOrderedList.c
@@ -1,8 +1,19 @@
+// Written by Joshua Hatton (z5215045) on 5/5/21
 
 #include "list.h"
 
+// Returns the number of duplicate elements
+// in an ordered linked list.
 int numDupesInOrderedList(List l) {
-	// TODO
-	return -1;
+	int dupes = 0;
+	Node curr = l->head;
+	while (curr != NULL && curr->next != NULL) {
+		if (curr->value == curr->next->value) {
+			dupes++;
+		}
+		curr = curr->next;
+	}
+
+	return dupes;
 }
 
