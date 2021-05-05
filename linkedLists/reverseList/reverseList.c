e6a4cc471aaa648f2bbbcd8db4242ca3a668132b
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-06 00:00:26 +1000

    Complete reverse list exercise

--- linkedLists/reverseList/reverseList.c
+++ linkedLists/reverseList/reverseList.c
@@ -1,7 +1,21 @@
+// Written by Joshua Hatton (z5215045) on 5/5/21
 
+#include <assert.h>
 #include "list.h"
 
+// Reverses a given singly linked list.
 void listReverse(List l) {
-	// TODO
+	assert(l != NULL);
+	Node prev = NULL;
+	Node curr = l->head;
+	Node next;
+	while (curr != NULL) {
+		if (curr->next == NULL) {
+			l->head = curr;
+		}
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
 }
-
