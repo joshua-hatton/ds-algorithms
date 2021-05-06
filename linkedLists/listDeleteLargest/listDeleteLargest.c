9baeb73cdee594262085afb606e178df57714256
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-06 15:23:44 +1000

    Complete delete largest exercise

--- linkedLists/listDeleteLargest/listDeleteLargest.c
+++ linkedLists/listDeleteLargest/listDeleteLargest.c
@@ -1,7 +1,31 @@
+// Written by Joshua Hatton (z5215045) on 6/5/21
 
 #include "list.h"
 
+// Deletes largest value in a linked list of positive
+// integers, and returns this value. Only first instance
+// of value is deleted.
 int listDeleteLargest(List l) {
-	return -1;
-}
+	Node curr = l->head;
+	Node largest = l->head;
+	int largestValue = curr->value;
+	Node beforeLargest = NULL;
 
+	while (curr != NULL && curr->next != NULL) {
+		if (curr->next->value > largestValue) {
+			largestValue = curr->next->value;
+			largest = curr->next;
+			beforeLargest = curr;
+		}
+		curr = curr->next;
+	}
+
+	if (beforeLargest != NULL) {
+		beforeLargest->next = largest->next;
+	} else {
+		l->head = largest->next;
+	}
+
+	free(largest);
+	return largestValue;
+}
