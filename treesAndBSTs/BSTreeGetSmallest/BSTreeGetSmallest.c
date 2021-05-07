f643004ca0cd8c3ae3d59de3abe493b4ed00990f
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 16:08:48 +1000

    Complete get smallest node exercise

--- treesAndBSTs/BSTreeGetSmallest/BSTreeGetSmallest.c
+++ treesAndBSTs/BSTreeGetSmallest/BSTreeGetSmallest.c
@@ -1,10 +1,30 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21 
 
 #include "BSTree.h"
 
 #include <stdlib.h>
 
+static BSTree doBSTreeGetSmallest(BSTree t, BSTree smallest);
+
+// Returns a pointer to the node containing 
+// the smallest value in the given BST.
 BSTree BSTreeGetSmallest(BSTree t) {
-	// TODO
-	return NULL;
+	if (t == NULL) {
+		return NULL;
+	}
+
+	BSTree smallest = t;
+	smallest = doBSTreeGetSmallest(t->left, smallest);
+
+	return smallest;
 }
 
+static BSTree doBSTreeGetSmallest(BSTree t, BSTree smallest) {
+	if (t != NULL) {
+		if (t->value < smallest->value) {
+			smallest = t;
+		}
+		smallest = doBSTreeGetSmallest(t->left, smallest);
+	}
+	return smallest;
+}
