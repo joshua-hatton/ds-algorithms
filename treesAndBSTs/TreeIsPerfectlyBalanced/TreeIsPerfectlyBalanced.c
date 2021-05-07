fe3abdbf55683024e3594b4a0649f0cc978d5d31
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 18:32:38 +1000

    Complete perfectly balanced exercise

--- treesAndBSTs/TreeIsPerfectlyBalanced/TreeIsPerfectlyBalanced.c
+++ treesAndBSTs/TreeIsPerfectlyBalanced/TreeIsPerfectlyBalanced.c
@@ -1,10 +1,27 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21
 
 #include <stdlib.h>
 
 #include "tree.h"
 
+static int treeSize(Tree t);
+
+// Determines if a tree is perfectly balanced.
 bool TreeIsPerfectlyBalanced(Tree t) {
-	// TODO
-	return false;
+	if (t == NULL) {
+		return true;
+	}
+	if (abs(treeSize(t->left) - treeSize(t->right)) > 1) {
+		return false;
+	}
+	return TreeIsPerfectlyBalanced(t->left) && 
+		   TreeIsPerfectlyBalanced(t->right);
 }
 
+// Returns size of a tree.
+static int treeSize(Tree t) {
+	if (t == NULL) {
+		return 0;
+	}
+	return 1 + treeSize(t->left) + treeSize(t->right);
+}
