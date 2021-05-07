b7802d14b29eaf6aba9941c12016403d69a12d96
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 20:51:15 +1000

    Complete DFS exercise

--- graphs/depthFirstSearch/depthFirstSearch.c
+++ graphs/depthFirstSearch/depthFirstSearch.c
@@ -1,10 +1,26 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "Graph.h"
 
+static void doDFS(Graph g, int v, bool *visited);
+
+// Performs a DFS using recursion. Prints out
+// vertices as they are visited.
 void depthFirstSearch(Graph g, int src) {
-	// TODO
+	bool *visited = calloc(GraphNumVertices(g), sizeof(bool));
+	doDFS(g, src, visited);
+	free(visited);
 }
 
+static void doDFS(Graph g, int v, bool *visited) {
+	printf("%d ", v);
+	visited[v] = true;
+	for (int w = 0; w < GraphNumVertices(g); w++) {
+		if (!visited[w] && GraphIsAdjacent(g, v, w)) {
+			doDFS(g, w, visited);
+		}
+	}
+}
