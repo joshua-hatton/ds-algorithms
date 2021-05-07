3c0b1605b18d12303743844aaaaeb55da4bc5e02
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 22:00:41 +1000

    Complete sum odds exercise

--- treesAndBSTs/TreeSumOdds/TreeSumOdds.c
+++ treesAndBSTs/TreeSumOdds/TreeSumOdds.c
@@ -1,9 +1,25 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21
+// Completed under time constraints
 
 #include <stdlib.h>
 
 #include "tree.h"
 
+static int doTreeSumOdd(Tree t, int sum);
+
+// Returns sum of all odd values.
 int TreeSumOdds(Tree t) {
-	return 0;
+	return doTreeSumOdd(t, 0);
 }
 
+static int doTreeSumOdd(Tree t, int sum) {
+	if (t == NULL) {
+		return sum;
+	}
+	if (abs(t->value) % 2 == 1) {
+		sum += t->value;
+	}
+	sum = doTreeSumOdd(t->left, sum);
+	sum = doTreeSumOdd(t->right, sum);
+	return sum; 
+}
