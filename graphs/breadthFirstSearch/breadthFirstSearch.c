40928353430bedcd467e8ae852691f90e3de40e2
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 23:38:21 +1000

    Complete BFS exercise

--- graphs/breadthFirstSearch/breadthFirstSearch.c
+++ graphs/breadthFirstSearch/breadthFirstSearch.c
@@ -1,3 +1,4 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,7 +6,26 @@
 #include "Graph.h"
 #include "Queue.h"
 
+// Performs BFS on a graph starting at a given 
+// source vertex. Prints out visited vertices.
 void breadthFirstSearch(Graph g, int src) {
-	// TODO
-}
+	bool *visited = calloc(GraphNumVertices(g), sizeof(bool));
+	visited[src] = true;
 
+	Queue q = QueueNew();
+	QueueEnqueue(q, src);
+	
+	while (!QueueIsEmpty(q)) {
+		int v = QueueDequeue(q);
+		printf("%d ", v);
+		for (int w = 0; w < GraphNumVertices(g); w++) {
+			if (!visited[w] && GraphIsAdjacent(g, v, w)) {
+				visited[w] = true;
+				QueueEnqueue(q, w);
+			}
+		}
+	}
+
+	free(visited);
+	QueueFree(q);
+}
