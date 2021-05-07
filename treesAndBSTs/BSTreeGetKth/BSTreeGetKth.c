605bebe9ee12a6a06b8ab5edfcb5e17f42b72285
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 18:19:48 +1000

    Upload solution

--- treesAndBSTs/BSTreeGetKth/BSTreeGetKth.c
+++ treesAndBSTs/BSTreeGetKth/BSTreeGetKth.c
@@ -1,10 +1,26 @@
+// Solution provided by CSE UNSW
 
 #include <stdlib.h>
 
 #include "BSTree.h"
 
+static int BSTreeSize(BSTree t);
+
 int BSTreeGetKth(BSTree t, int k) {
-	// TODO
-	return -42;
+	int leftSize = BSTreeSize(t->left);
+	if (k == leftSize) {
+		return t->value;
+	} else if (k < leftSize) {
+		return BSTreeGetKth(t->left, k);
+	} else {
+		return BSTreeGetKth(t->right, k - leftSize - 1);
+	}
 }
 
+static int BSTreeSize(BSTree t) {
+	if (t == NULL) {
+		return 0;
+	} else {
+		return 1 + BSTreeSize(t->left) + BSTreeSize(t->right);
+	}
+}
