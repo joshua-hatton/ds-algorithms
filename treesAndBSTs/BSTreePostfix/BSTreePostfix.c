f5132896299ddfe80b0f7b6d5fc133099b39e783
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 12:38:37 +1000

    Complete postfix exercise

--- treesAndBSTs/BSTreePostfix/BSTreePostfix.c
+++ treesAndBSTs/BSTreePostfix/BSTreePostfix.c
@@ -1,9 +1,15 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21
 
 #include <stdio.h>
 
 #include "BSTree.h"
 
+// Prints out values of a BST in postfix order.
 void BSTreePostfix(BSTree t) {
-	// TODO
+	if (t == NULL) {
+		return;
+	} 
+	BSTreePostfix(t->left);
+	BSTreePostfix(t->right);
+	printf("%d ", t->value);
 }
-
