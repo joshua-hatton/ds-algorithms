b5b69985993322c766a99409d46754f6747fcd78
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-07 17:15:27 +1000

    Complete tree height exercise

--- treesAndBSTs/TreeHeight/TreeHeight.c
+++ treesAndBSTs/TreeHeight/TreeHeight.c
@@ -1,9 +1,17 @@
+// Written by Joshua Hatton (z5215045) on 7/5/21
 
 #include <stdlib.h>
 
 #include "tree.h"
 
+// Returns height of the given tree.
 int TreeHeight(Tree t) {
-    return -1;
-}
+    if (t == NULL) {
+        return -1;
+    }
 
+    int leftHeight = TreeHeight(t->left);
+    int rightHeight = TreeHeight(t->right);
+
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+}
