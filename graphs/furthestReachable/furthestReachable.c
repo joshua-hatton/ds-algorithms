86533b66cacc5009388b9b963d4e40c3a53b3a42
Joshua Hatton <z5215045@ad.unsw.edu.au>
2021-05-08 00:30:01 +1000

    Begin furthest reachable exercise

--- graphs/furthestReachable/furthestReachable.c
+++ graphs/furthestReachable/furthestReachable.c
@@ -1,3 +1,4 @@
+// Written by Joshua Hatton (z5215045) on 8/5/21
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +6,10 @@
 #include "Graph.h"
 #include "Queue.h"
 
+// Returns the furthest vertex reachable from
+// a given source vertex. If there are multiple
+// such vertices, the largest valued one is returned.
+// If source is isolated, source is returned.
 int furthestReachable(Graph g, int src) {
 	// TODO
 	return -1;
